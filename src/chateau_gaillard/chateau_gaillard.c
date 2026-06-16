1ea20d64625601fcc4212e4dc57626369851fed2
killeroonie <rob.ross@gmail.com>
2026-06-15 18:48:20 -0700

    Moved declarations from header into C file so we can delete the header file.

--- src/chateau_gaillard/chateau_gaillard.c
+++ src/chateau_gaillard/chateau_gaillard.c
@@ -27,9 +27,88 @@ clang -g -DCHATEAU_GAILLARD_MAIN -fsanitize=address -fsanitize=leak -Wall -Werro
 
 */
 
-#include "chateau_gaillard.h"
-
 #include <limits.h>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "../adventure_shared.h"
+#include "../directions.h"
+#include "../common/console_utils.h"
+#include "../mersenne_twister.h"
+#include "../rooms.h"
+#include "../monsters.h"
+#include "../objects.h"
+#include "../parser.h"
+
+
+char const * const VALID_COMMANDS = "HIQATRFPGNSEWUDLM12";
+
+constexpr int BARE_HANDS           =  0;
+
+constexpr int OBJECT_AXE           =  1;
+constexpr int OBJECT_SWORD         =  2;
+constexpr int OBJECT_DAGGER        =  3;
+constexpr int OBJECT_MACE          =  4;
+constexpr int OBJECT_QUARTER_STAFF =  5;
+constexpr int OBJECT_MORNING_STAR  =  6;
+constexpr int OBJECT_FALCHION      =  7;
+constexpr int OBJECT_AMULET        =  9;
+
+constexpr int OBJECT_MYSTIC_SCROLL  = 12;
+constexpr int OBJECT_HEALING_POTION = 13;
+
+constexpr int OBJECT_DIADEM      = 16;
+constexpr int OBJECT_SILVER_KEY  = 17;
+constexpr int OBJECT_GOLD_KEY    = 18;
+constexpr int OBJECT_STONE_CHEST = 19;
+constexpr int OBJECT_IRON_CHEST  = 20;
+
+constexpr int NUM_TREASURES     = 21;
+
+constexpr int NUM_DEATH_ROOMS    =  6;
+
+
+constexpr int ROOM_START         = 27;
+constexpr int ROOM_END           = 28;
+
+// these constants are nice for static compiler checks but won't scale to a real world app. We're using these constants
+// to add things to a room, (treasure, monster), exclude things from being added, check special conditions, e.g.,
+// do you have the right key to unlock the door, etc. These should all be pushed into the data layer.
+constexpr int ROOM_MAGICIAN      =  2;
+constexpr int ROOM_MATTRESS      =  3;
+constexpr int ROOM_WOODEN        =  4;
+constexpr int ROOM_STONE         =  5;  // death
+constexpr int ROOM_L_SHAPED      =  6;
+
+constexpr int ROOM_KITCHEN       =  8;
+constexpr int ROOM_CHARISMA_REDUCE = 13;
+constexpr int ROOM_YELLOW        = 16;
+constexpr int ROOM_CRAMPED       = 17;
+constexpr int ROOM_TRAPPED       = 29;  // death
+constexpr int ROOM_PIT_OF_FLAMES = 30;  // death
+constexpr int ROOM_ACID          = 31;  // death
+constexpr int ROOM_SPIDER        = 32;  // death
+constexpr int ROOM_UNEVEN        = 34;
+
+constexpr int ROOM_DUNGEON       = 36;
+constexpr int ROOM_GARGOYLE      = 37;  // death
+constexpr int ROOM_TROPHY        = 40;
+constexpr int ROOM_SECRET_ROOM   = 41;
+
+constexpr int ROOM_TURRET        = 44;
+
+constexpr int MONSTER_DWARF = 1;
+
+const int MAX_ROOM_OBJECTS =  10; //maximum number of items that can be placed in a room
+const int MAX_PLAYER_OBJECTS = 5; // max number of items that can be carried
+
+
+bool perform_action(GameState *gs, enum Command  cmd,  int arg1,  int arg2,  int arg3);
+
 
 int ROOM_GRAPH[][RGINDEX_COUNT] = {
     {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },  //  NULL ROOM 0
