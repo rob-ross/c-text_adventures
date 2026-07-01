877e0f6a321bd18457655d0c1b2d7c28db9967ff
killeroonie <rob.ross@gmail.com>
2026-06-30 23:03:58 -0700

    Removed unused code.

--- src/common/files.h
+++ src/common/files.h
@@ -17,25 +17,14 @@ extern "C" {
 #endif
 
 
-constexpr size_t ONE_MEBIBYTE = 1024 * 1024;
-constexpr size_t ONE_GIBIBYTE = ONE_MEBIBYTE * 1024;
-
-typedef struct value_pos_s {
-    LenStr value;
-    const char* file_name;
-    int    start_char_pos;
-    int    end_char_pos;
-    int    line_num_start;
-    int    col_start;
-    int    line_num_end;
-    int    col_end;
-} ValuePos;
-
+// declares a function pointer type, `file_process_action`
+// the function takes a file pointer and a handle to a void to return the result of the
+// function operation. The function returns an int error code, 0 if no error.
 typedef int (*file_process_action)( FILE *fptr, void **result_ptr);
 
-
-int process_file(string file_name, file_process_action function, void **result_ptr);
-void free_LenStrArray(LenStrArray *lsa);
+// resource guard function. Wraps the function call between calls to open and close
+// to ensure the file is always cleaned up.
+int process_file(char const * file_name, file_process_action function, void **result_ptr);
 
 #ifdef __cplusplus
 }
