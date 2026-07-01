1ce515c698c9defa4180df481290bbc4922373c8
killeroonie <rob.ross@gmail.com>
2026-07-01 00:07:56 -0700

    Moved global typedefs to roblib/roblib_types.h. Just kept text_adventure-specific typedefs

--- src/common/base_types.h
+++ src/common/base_types.h
@@ -12,18 +12,15 @@
 #ifndef TEXT_ADVENTURES_BASE_TYPES_H
 #define TEXT_ADVENTURES_BASE_TYPES_H
 
+#include <roblib/roblib_types.h>
+
 #include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef uint8_t  byte;
-typedef uint32_t u32;
-typedef int32_t  s32;
-typedef uint64_t u64;
-typedef int64_t  s64;
-
 
 typedef s32 room_id;
 typedef s32 monster_id;
